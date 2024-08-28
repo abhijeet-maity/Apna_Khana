5f63d48a7bdc9c1ec6ca4d8cceeddd0747ca97c5
abhijeet-maity <abhijeetmaity1000@gmail.com>
2024-08-28 18:16:27 +0530

    validated to proceed further if user is not logged in

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,11 +1,45 @@
-import { useContext } from 'react'
-import './Cart.css'
+import { useContext, useState, useEffect } from 'react';
+import './Cart.css';
 import { StoreContext } from '../../Context/StoreContext'
 import {useNavigate} from 'react-router-dom'
+import {auth, db} from "../../fireBase/firebase.jsx";
+import {getDoc, doc} from "firebase/firestore";
+import { toast } from 'react-toastify';
 
-const Cart = () => {
+const Cart = ({setShowLogin}) => {
   const navigate = useNavigate();
   const {cartItems, food_list, removeFromCart, totalCartAmount} = useContext(StoreContext);
+  const [userDetails, setUserDetails] = useState(null);
+
+  const fetchUserData = async () => {
+    auth.onAuthStateChanged(async (user) => {
+      console.log(user);
+      const docRef = doc(db, "Users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if(docSnap.exists()) {
+        setUserDetails(docSnap.data());
+        console.log(docSnap.data());
+      } else {
+        console.log("User is not logged in");
+        toast.error("User is not logged in", {
+          position : "bottom-center",
+        })
+      }
+    });
+  };
+
+  const alertUserToLogin = () => {
+    console.log("You have to Login to Proceed further");
+    toast.error("You have to Login to Proceed further",{
+      position : "top-center",
+    })
+    setShowLogin(true);
+  }
+
+  useEffect(() => {
+    fetchUserData();
+  },[]);
+
   return (
     <div className='cart'>
       <div className="cartItems">
@@ -56,7 +90,10 @@ const Cart = () => {
               <b>&#8377;  {totalCartAmount()===0?0:totalCartAmount() + 40}</b>
             </div>
           </div>
-          <button onClick={()=>navigate('/PlaceOrder')}>PROCEED TO CHECKOUT</button>
+          {userDetails ? (<button onClick={()=>navigate('/PlaceOrder')}>PROCEED TO CHECKOUT</button>) : (
+            <button onClick={alertUserToLogin}>PROCEED TO CHECKOUT</button>
+          )}
+          
         </div>
         <div className="promocode">
           <div className="promocode-details">
